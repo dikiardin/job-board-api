55a077fd030feee30e4dee859e9e4b68613b6ee3
nicholas-0101 <nsvertian@gmail.com>
2025-09-22 13:35:43 +0700

    connect social login with employee and admin table (#6)

--- src/repositories/user/userProvider.repository.ts
+++ src/repositories/user/userProvider.repository.ts
@@ -1,7 +1,7 @@
 import { prisma } from "../../config/prisma";
 export class UserProviderRepo {
   static async findByProvider(
-    provider: "GOOGLE" | "APPLE" | "MICROSOFT",
+    provider: "GOOGLE" | "FACEBOOK",
     providerId: string
   ) {
     return prisma.userProvider.findUnique({
@@ -13,14 +13,16 @@ export class UserProviderRepo {
   static async createUserWithProvider(data: {
     name: string;
     email: string;
-    provider: "GOOGLE" | "APPLE" | "MICROSOFT";
+    provider: "GOOGLE" | "FACEBOOK";
     providerId: string;
+    role: "USER" | "ADMIN";
   }) {
     return prisma.user.create({
       data: {
         name: data.name,
         email: data.email,
         passwordHash: "", 
+        role: data.role,
         isVerified: true,
         providers: {
           create: { provider: data.provider, providerId: data.providerId },
